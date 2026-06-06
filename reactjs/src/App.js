f417f145742b5f1e1f4fb46dd371e65cb650b024
SagiruddinAkhtar0786 <kp19nimcet20@gmail.com>
2026-06-06 23:10:22 +0530

    added react router dom

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,23 +1,62 @@
 //import logo from './logo.svg';
 import Navbar from './CustomComponents/Navbar';
 import CustomForm from './CustomComponents/CustomForm';
-//import About from './CustomComponents/About';
+import About from './CustomComponents/About';
+import React, { useState } from 'react';
+import Alert from './CustomComponents/Alert';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
-  
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#19196e';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
+
+    }
+  };
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About" />
-
+      <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About" />
+      <Alert alert={alert} />
       <div className="container my-3" >
-           <CustomForm heading="Enter the text to Analyze below"/>
-      {/* <About/> */}
-   </div>
-      
-    </>
 
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<CustomForm showAlert={showAlert} heading="Enter the text to Analyze below" mode={mode} />} />
+        </Routes>
+        {/* <About/>*/}
+      </div>
+    </Router >
+    </>
   );
 }
 
--- reactjs/src/CustomComponents/CustomForm.js
+++ reactjs/src/CustomComponents/CustomForm.js
@@ -4,12 +4,13 @@ import React, {useState}from 'react'
 export default function CustomForm(props) {
 
 //const [text, setText] = useState("Enter text here"); // useState is a hook which is used to change the state of the component and also to re-render the component when the state changes.   
-const [text, setText] = useState("dfghjkl;");
+const [text, setText] = useState("");
 
 const handleUpClick = () => {
     console.log("UpperCase was clicked" + text);
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Converted to UpperCase", "success");
 }
 const handleOnChange = (event) => {
     console.log("On Change");
@@ -21,11 +22,13 @@ const handleLoClick = () => {
     console.log("LowerCase was clicked" + text);
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Converted to LowerCase", "success");
 }
 const handleClearClick = () => {
     console.log("Clear Text was clicked" + text);
     let newText = "";
     setText(newText);
+    props.showAlert("Text cleared", "success");
 }   
 
 
@@ -35,11 +38,11 @@ const handleClearClick = () => {
 
         <>
         
-        <div>
+        <div className='container' style={{color: props.mode === 'dark' ? 'blue' : '#0b0b36' }}>
             <h1 className="props_heading" >{props.heading} </h1>
             <div >
               
-                <textarea className="form-control" value = {text} id="myBox" rows="3" onChange={handleOnChange}></textarea>
+                <textarea className="form-control" value = {text} style={{backgroundColor:props.mode === 'dark' ? 'grey' : 'white' , color: props.mode === 'dark' ? 'white' : '#737381'    }} id="myBox" rows="3" onChange={handleOnChange}></textarea>
             </div>
 
             <button className=" btn btn-primary mx-2 my-2" onClick={handleUpClick}>Conver to UpperCase</button>
@@ -48,12 +51,12 @@ const handleClearClick = () => {
 
         </div>
 
-        <div className="container">
+        <div className="container my-3" style={{color: props.mode === 'dark' ? 'white' : '#1f1f5b' }}>
             <h2>your text here</h2>
             <p>{text.split(" ").length}  words and  {text.length} characters </p>
             <p>{0.008 * text.split(" ").length} minutes to read</p>
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length > 0 ? text : "Enter something to preview"}</p>
         </div>
 
         </>
